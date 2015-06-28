62a23ef87b3769d2938add68fb4d4a3e5dc5829b
AyanIslam <ayanislam@Ayans-MacBook-Pro-3.local>
2015-06-28 19:14:44 -0400

    Added week 2 homework folder

---
+++ Homework/Week 2/Code/Lesson02/FibonacciAdder.swift
@@ -0,0 +1,28 @@
+//
+//  FibonacciAdder.swift
+//  Lesson02
+//
+//  Created by Ayan Islam on 6/28/15.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
+//
+
+import Foundation
+
+class FibonacciAdder {
+    
+    func numberAtIndex(number: Int) -> Int {
+        
+        var retVal: Int = 0
+        
+        if number == 0 {
+            retVal = 0
+        } else if number == 1 {
+            retVal = 1
+        } else {
+            for i in 2...number {
+                retVal = numberAtIndex(i-1) + numberAtIndex(i-2)
+            }
+        }
+        return retVal
+    }
+}
