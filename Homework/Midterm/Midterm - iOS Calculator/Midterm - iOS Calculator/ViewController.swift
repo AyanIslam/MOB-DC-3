8541275b88aa0ed7b7403bd4729d6d6b33e6d4db
AyanIslam <ayanislam@Ayans-MacBook-Pro-2.local>
2015-07-19 12:22:11 -0400

    updated files with midterm in progress

---
+++ Homework/Midterm/Midterm - iOS Calculator/Midterm - iOS Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Midterm - iOS Calculator
+//
+//  Created by Ayan Islam on 7/18/15.
+//  Copyright (c) 2015 Ayan Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
