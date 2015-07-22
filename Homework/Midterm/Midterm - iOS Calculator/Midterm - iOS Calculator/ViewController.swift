6abc6048f503f7ebe2d40aa1eb58f60527ed118f
AyanIslam <ayanislam@Ayans-MacBook-Pro-2.local>
2015-07-22 18:15:47 -0400

    updated with midterm in the HW folder

--- Homework/Midterm/Midterm - iOS Calculator/Midterm - iOS Calculator/ViewController.swift
+++ Homework/Midterm/Midterm - iOS Calculator/Midterm - iOS Calculator/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var screenField: UILabel!
+    var firstNumber = Float()
+    var secondNumber = Float()
+    var result = Float()
+    var typingNumber = false
+    var operationButton = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func numberButton(sender: AnyObject) {
+        var numberButton = sender.currentTitle
+        if typingNumber == true {
+            screenField.text = screenField.text! + numberButton!!;
+        } else {
+            screenField.text = numberButton;
+        }
+        typingNumber = true
+    }
+    
+    @IBAction func operationButton(sender: AnyObject) {
+        operationButton = sender.currentTitle!!
+        firstNumber = (screenField.text! as NSString).floatValue
+        typingNumber = false
+    }
+    
+    @IBAction func decimalButton(sender: AnyObject) {
+        var decimalButton = sender.currentTitle
+        screenField.text = screenField.text! + decimalButton!!
+    }
+    
+    @IBAction func equalButton(sender: AnyObject) {
+        secondNumber = (screenField.text! as NSString).floatValue
+        if operationButton == "+" {
+            result = firstNumber + secondNumber
+        } else if operationButton == "-" {
+            result = firstNumber - secondNumber
+        } else if operationButton == "x" {
+            result = firstNumber * secondNumber
+        } else  if operationButton == "/"{
+            result =  firstNumber / secondNumber
+        } else if operationButton == "%" {
+            result = (firstNumber * secondNumber) / 100
+        } else {
+        screenField.text = "\(result)"
+        }
+    }
 
+    @IBAction func clearButton(sender: AnyObject) {
+        firstNumber = 0
+        secondNumber = 0
+        result = 0
+        screenField.text = "\(result)"
+    }
+    
 }
 
