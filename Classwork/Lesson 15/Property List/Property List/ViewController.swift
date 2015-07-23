039d1b86a5bc6cfbbbc2d8aebf2e19acca817a22
AyanIslam <ayanislam@Ayans-MacBook-Pro-2.local>
2015-07-22 20:49:55 -0400

    update

---
+++ Classwork/Lesson 15/Property List/Property List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Property List
+//
+//  Created by Ayan Islam on 7/22/15.
+//  Copyright (c) 2015 Ayan Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
